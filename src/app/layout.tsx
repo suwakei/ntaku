d538116186d541a99e4e8707ad28e7efb91bd7b9
keitosuwahara <keitosuwahara0816@gmail.com>
2025-10-31 17:19:33 +0900

    added config of card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Header from '@/components/layout/Header';
-import { environments } from '@/envs.ts'
+import { environments } from '@/envs.ts';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -14,10 +14,10 @@ const geistMono = Geist_Mono({
 });
 
 const title = 'N択 - 迷った時の最終兵器！';
-const description = 'N択は、複数の選択肢で迷った時にランダムで一つを選んでくれるシンプルなWebアプリケーションです。';
+const description =
+  'N択は、複数の選択肢で迷った時にランダムで一つを選んでくれるシンプルなWebアプリケーションです。';
 const appUrl = environments.APP_URL;
 const ogpImageUrl = `${appUrl}/ntakuLogo.png`;
-
 
 export const metadata: Metadata = {
   title: title,
@@ -44,15 +44,15 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: 'N択ロゴ',
-      }
+      },
     ],
   },
   twitter: {
     card: 'summary_large_image',
     title: title,
     description: description,
-    images: [ogpImageUrl]
-  }
+    images: [ogpImageUrl],
+  },
 };
 
 export default function RootLayout({
